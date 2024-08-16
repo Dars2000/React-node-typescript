46d2889fec9f9517515c6ea70c14b7897297b818
Diego Robertis <128153675+Dars2000@users.noreply.github.com>
2024-08-16 12:39:29 -0400

    Update index.tsx

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css'; // Importa el archivo CSS global
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
